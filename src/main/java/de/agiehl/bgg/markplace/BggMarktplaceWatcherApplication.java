a58e5f3d9f741d45dfdff139d6bb2c57db288d76
Jens Giehl <mobil.jgiehl@gmail.com>
2023-06-02 22:30:33 +0200

    Init commit (start.spring.io)

---
+++ src/main/java/de/agiehl/bgg/markplace/BggMarktplaceWatcherApplication.java
@@ -0,0 +1,13 @@
+package de.agiehl.bgg.markplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BggMarktplaceWatcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BggMarktplaceWatcherApplication.class, args);
+	}
+
+}
